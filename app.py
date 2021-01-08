92ca0afd3a9fcfaa46fc73593e2659932ac93bea
dbhuller <dbhuller@mail.sfsu.edu>
2021-01-08 11:33:02 -0800

    added hashing for password in database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from passlib.hash import pbkdf2_sha256
 from wtform_fields import *
 from models import *
 
@@ -18,8 +19,11 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+        # hashed password
+        hashed_password = pbkdf2_sha256.hash(password)
+
         # Add user to DB
-        user = User(username=username, password=password)
+        user = User(username=username, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
